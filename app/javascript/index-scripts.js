f87f717aef5c0c7bc38a719f5e25517d4d78cccf
Victor Zenin <victor.zenin.o@gmail.com>
2019-10-05 15:56:23 +0300

    front

--- app/javascript/index-scripts.js
+++ app/javascript/index-scripts.js
@@ -1,14 +1,27 @@
 $(document).ready(function() {
-    $(".showing_btn").click(function(event) {
-      event.preventDefault();
-      $('.formDiv').show();
-    })
-    $(".hiding_btn").click(function(event) {
-      event.preventDefault();
-      $('.formDiv').hide();
-    })
-    $(".submit_link").click(function(event) {
-      event.preventDefault();
-      $("#new_todo_form").submit();
-    })
-  });
+  $(".showing_btn").click(function(event) {
+    event.preventDefault();
+    $('.formDiv').css({'display': 'inline-block'});
+    $('.formDiv-wrapper').css({'display': 'flex'});
+  })
+  $(".hiding_btn").click(function(event) {
+    event.preventDefault();
+    $('.formDiv').hide();
+    $('.formDiv-wrapper').hide();
+  })
+  $(".submit_link").click(function(event) {
+    event.preventDefault();
+    $("#new_todo_form").submit();
+  })
+  $('input').iCheck({
+    checkboxClass: 'icheckbox_square-blue'
+  })
+  $('input').on('ifChanged', function(event) {
+    $("#" + event.target.parentNode.parentNode.id).submit();
+  })
+  $('.project-selection').select2({
+    placeholder: 'Категория',
+    minimumResultsForSearch: -1,
+    width:'100%',
+  })
+});
