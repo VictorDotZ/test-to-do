5c82126a2a4b81324ace56e5f92aadd4c0f15860
Victor Zenin <victor.zenin.o@gmail.com>
2019-10-05 15:58:39 +0300

    work with empty project_id (placeholder in dropdown returns empty id string), logic for todos status updating.

--- app/controllers/todos_controller.rb
+++ app/controllers/todos_controller.rb
@@ -1,21 +1,30 @@
 class TodosController < ApplicationController
   def index
-    @projects = Project.all
+    @projects = Project.includes(:todos)
     @todo = Todo.new
   end
 
   def create
-    @todo = Todo.new({
-      :text => todo_params[:text],
-      :project => Project.find(todo_params[:project_id])
-      })
-    if (todo_params[:text] != "")
-      @todo.save
+    if !todo_params[:project_id].empty?
+      @todo = Todo.new({
+        :text => todo_params[:text],
+        :project => Project.find(todo_params[:project_id])
+        })
+      if (todo_params[:text] != "")
+        @todo.save
+      end
     end
     redirect_to root_path
   end
 
   def update
+    @todo = Todo.find(params[:todo_id])
+    if @todo.isCompleted != true
+      @todo.isCompleted = true
+    else
+      @todo.isCompleted = false
+    end
+    @todo.save
   end
 
   private def todo_params
